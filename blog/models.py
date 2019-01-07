c32d31b4675dc2bfd80dcd32aad14948601a4165
Sarah Kirk <kirkse710@gmail.com>
2019-01-07 10:35:06 -0800

    made migrations, made super user

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.Charfield(max_length=200)
+    title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
